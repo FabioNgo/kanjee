2718eae8f7c0a531ad8da529d76176ae7e9495a7
Fabio Ngo <ngolebaotrung@gmail.com>
2014-05-29 13:49:44 +0700

    Create basic classes

---
+++ kanjee/src/uet/kanjee/KCharacter.java
@@ -0,0 +1,114 @@
+package uet.kanjee;
+
+import java.util.ArrayList;
+
+public class KCharacter {
+	private int id;
+	private int numStrokes;
+	private String text;
+	private String pinyin;
+	private String meaning;
+	private String other;
+	private ArrayList<KRadical> relatedRadicals;
+	public KCharacter(){
+		setId(-1);
+		setNumStrokes(-1);
+		setText("");
+		setPinyin("");
+		setMeaning("");
+		setOther("");
+		setRelatedRadicals(new ArrayList<KRadical>());
+	}
+	public KCharacter(int id, int numStrokes, String text, String pinyin, String meaning, String other){
+		this.setId(id);
+		this.setNumStrokes(numStrokes);
+		this.setText(text);
+		this.setPinyin(pinyin);
+		this.setMeaning(meaning);
+		this.setOther(other);
+	}
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the numStrokes
+	 */
+	public int getNumStrokes() {
+		return numStrokes;
+	}
+	/**
+	 * @param numStrokes the numStrokes to set
+	 */
+	public void setNumStrokes(int numStrokes) {
+		this.numStrokes = numStrokes;
+	}
+	/**
+	 * @return the text
+	 */
+	public String getText() {
+		return text;
+	}
+	/**
+	 * @param text the text to set
+	 */
+	public void setText(String text) {
+		this.text = text;
+	}
+	/**
+	 * @return the pinyin
+	 */
+	public String getPinyin() {
+		return pinyin;
+	}
+	/**
+	 * @param pinyin the pinyin to set
+	 */
+	public void setPinyin(String pinyin) {
+		this.pinyin = pinyin;
+	}
+	/**
+	 * @return the meaning
+	 */
+	public String getMeaning() {
+		return meaning;
+	}
+	/**
+	 * @param meaning the meaning to set
+	 */
+	public void setMeaning(String meaning) {
+		this.meaning = meaning;
+	}
+	/**
+	 * @return the other
+	 */
+	public String getOther() {
+		return other;
+	}
+	/**
+	 * @param other the other to set
+	 */
+	public void setOther(String other) {
+		this.other = other;
+	}
+	/**
+	 * @return the relatedRadicals
+	 */
+	public ArrayList<KRadical> getRelatedRadicals() {
+		return relatedRadicals;
+	}
+	/**
+	 * @param relatedRadicals the relatedRadicals to set
+	 */
+	public void setRelatedRadicals(ArrayList<KRadical> relatedRadicals) {
+		this.relatedRadicals = relatedRadicals;
+	}
+}
---
+++ kanjee/src/uet/kanjee/KRadical.java
@@ -0,0 +1,72 @@
+package uet.kanjee;
+
+import java.util.ArrayList;
+
+public class KRadical {
+	private int id;
+	private int numStrokes;
+	private String imagePath;
+	private ArrayList<KCharacter> relatedCharacters;
+	public KRadical() {
+		// TODO Auto-generated constructor stub
+		setId(-1);
+		setNumStrokes(-1);
+		setImagePath("");
+		setRelatedCharacters(new ArrayList<KCharacter>());
+	}
+	public KRadical(int id, int numStrokes, String imagePath){
+		this.setId(id);
+		this.setNumStrokes(numStrokes);
+		this.setImagePath(imagePath);
+	}
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the numStrokes
+	 */
+	public int getNumStrokes() {
+		return numStrokes;
+	}
+	/**
+	 * @param numStrokes the numStrokes to set
+	 */
+	public void setNumStrokes(int numStrokes) {
+		this.numStrokes = numStrokes;
+	}
+	/**
+	 * @return the imagePath
+	 */
+	public String getImagePath() {
+		return imagePath;
+	}
+	/**
+	 * @param imagePath the imagePath to set
+	 */
+	public void setImagePath(String imagePath) {
+		this.imagePath = imagePath;
+	}
+	/**
+	 * @return the relatedCharacters
+	 */
+	public ArrayList<KCharacter> getRelatedCharacters() {
+		return relatedCharacters;
+	}
+	/**
+	 * @param relatedCharacters the relatedCharacters to set
+	 */
+	public void setRelatedCharacters(ArrayList<KCharacter> relatedCharacters) {
+		this.relatedCharacters = relatedCharacters;
+	}
+	
+
+}
