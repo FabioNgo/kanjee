64061f3524edac3005be1c3cb3c4f9bebbf42221
Fabio Ngo <ngolebaotrung@gmail.com>
2014-05-30 13:14:32 +0700

    update

--- kanjee/src/uet/kanjee/DatabaseHelper.java
+++ kanjee/src/uet/kanjee/DatabaseHelper.java
@@ -28,107 +28,65 @@ import android.widget.Toast;
  */
 public class DatabaseHelper extends SQLiteOpenHelper {
 	static String DB_NAME = "kanjee.sqlt";
-	
+
 	public SQLiteDatabase myDatabase;
 	Context myContext;
+
 	/**
 	 * @param context
 	 * @param name
 	 * @param factory
 	 * @param version
-	 * @throws IOException 
+	 * @throws IOException
 	 */
-	
+
 	public DatabaseHelper(Context context) throws IOException {
-		
-		super(context, context.getExternalFilesDir(context.ACCESSIBILITY_SERVICE).toString(), null, 1);
-		
+
+		super(context, context.getExternalFilesDir(
+				context.ACCESSIBILITY_SERVICE).toString(), null, 1);
+
 		this.myContext = context;
-		copyDatabase();
-		myDatabase = SQLiteDatabase.openDatabase(context.getExternalFilesDir(context.ACCESSIBILITY_SERVICE).toString() + "/" + DB_NAME,
-												null, SQLiteDatabase.OPEN_READONLY);
+		myDatabase = SQLiteDatabase.openDatabase(
+				context.getExternalFilesDir(context.ACCESSIBILITY_SERVICE)
+						.toString() + "/" + DB_NAME, null,
+				SQLiteDatabase.OPEN_READONLY);
 		// TODO Auto-generated constructor stub
-	}	
-	private void copyDatabase() throws IOException {
-		// TODO Auto-generated method stub
-		InputStream myinput = myContext.getAssets().open(DB_NAME);
-
-        // Path to the just created empty db
-
-        //Open the empty db as the output stream
-		File outputFile = new File(myContext.getExternalFilesDir(myContext.ACCESSIBILITY_SERVICE),DB_NAME);
-		
-        OutputStream output = new FileOutputStream(outputFile);
-
-        // transfer byte to inputfile to outputfile
-        byte[] buffer = new byte[1024];
-        int length;
-        while ((length = myinput.read(buffer))>0) {
-            output.write(buffer,0,length);
-        }
-
-        //Close the streams
-        output.flush();
-        output.close();
-        myinput.close();
-		
 	}
-
-
 
 	
-
+	
+	/**
+	 * 
+	 * @return the array of all characters in database
+	 */
 	public List<KCharacter> getAllChars() {
-	    List<KCharacter> chars = new ArrayList<KCharacter>();
-	    String selectQuery = "SELECT  * FROM " + "CHARACTERS";
-	 
-	    
-	 
-	    
-	    Cursor c = myDatabase.rawQuery(selectQuery, null);
-	 
-	    // looping through all rows and adding to list
-	    if (c.moveToFirst()) {
-	    	int a = 0;
-	        do {
-	        	
-	            KCharacter character = new KCharacter(c.getInt(c.getColumnIndex("ID")),
-	            							c.getInt(c.getColumnIndex("NUMSTROKES")),
-	            							c.getString(c.getColumnIndex("TEXT")),
-	            							c.getString(c.getColumnIndex("PINYIN")),
-	            							c.getString(c.getColumnIndex("MEANING")),
-	            							c.getString(c.getColumnIndex("OTHER"))
-	            		);
-	            
-	 
-	            // adding to tags list
-	            chars.add(character);
-	            a++;
-	            Toast.makeText(myContext, String.valueOf(a), Toast.LENGTH_SHORT).show();
-	            
-	        } while (c.moveToNext());
-	    }
-	    return chars;
+		List<KCharacter> chars = new ArrayList<KCharacter>();
+		String selectQuery = "SELECT  * FROM " + "CHARACTERS";
+		Cursor c = myDatabase.rawQuery(selectQuery, null);
+		// looping through all rows and adding to list
+		if (c.moveToFirst()) {
+			int a = 0;
+			do {
+				KCharacter character = new KCharacter(c.getInt(c
+						.getColumnIndex("ID")), c.getInt(c
+						.getColumnIndex("NUMSTROKES")), c.getString(c
+						.getColumnIndex("TEXT")), c.getString(c
+						.getColumnIndex("PINYIN")), c.getString(c
+						.getColumnIndex("MEANING")), c.getString(c
+						.getColumnIndex("OTHER")));
+				// adding to tags list
+				chars.add(character);
+			} while (c.moveToNext());
+		}
+		return chars;
 	}
 
-	private boolean checkDatabase() {
-		// TODO Auto-generated method stub   
-            try {
-				InputStream dbfile = myContext.getAssets().open(DB_NAME);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				return false;
-			}
-            //checkdb = SQLiteDatabase.openDatabase(myPath,null,SQLiteDatabase.OPEN_READWRITE);
-        
-        return true;
-		
-	}
-
-
-
-	/* (non-Javadoc)
-	 * @see android.database.sqlite.SQLiteOpenHelper#onCreate(android.database.sqlite.SQLiteDatabase)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * android.database.sqlite.SQLiteOpenHelper#onCreate(android.database.sqlite
+	 * .SQLiteDatabase)
 	 */
 	@Override
 	public void onCreate(SQLiteDatabase arg0) {
@@ -136,8 +94,12 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
 	}
 
-	/* (non-Javadoc)
-	 * @see android.database.sqlite.SQLiteOpenHelper#onUpgrade(android.database.sqlite.SQLiteDatabase, int, int)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * android.database.sqlite.SQLiteOpenHelper#onUpgrade(android.database.sqlite
+	 * .SQLiteDatabase, int, int)
 	 */
 	@Override
 	public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) {
