88d14f4e38a847c674ae6dfdb9d22a7dcc06a049
Fabio Ngo <ngolebaotrung@gmail.com>
2014-06-07 11:12:40 +0700

    fix libary

--- kanjee/src/uet/kanjee/MainActivity.java
+++ kanjee/src/uet/kanjee/MainActivity.java
@@ -7,9 +7,9 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+
+
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -24,7 +24,7 @@ import android.os.Build;
  * @author Huy
  *
  */
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 	DatabaseHelper db;
 	static final String DB_NAME = "kanjee.sqlt";
 	@Override
@@ -48,25 +48,7 @@ public class MainActivity extends ActionBarActivity {
 		tv.setText(String.valueOf(chars.size()));
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+	
 	
 	/**
 	 * copy database file from assets to storage
